b039a4f2e7b649193da79aef012163a0cc28132a
hoang duy <hoangduy1771@gmail.com>
2022-10-26 23:43:02 +0700

    start 2 lessons

---
+++ Lessons-brocode/1-set-up-env.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE myDB;
+USE myDB;
+-- DROP DATABASE mydb;
+
+-- Modify database to read only == 1
+-- After read only == 1, database cant drop 
+ALTER DATABASE myDB READ ONLY = 0;
+
+-- Need to install keyring to work with encrypted database
+ALTER DATABASE myDB ENCRYPTION = 'Y';
+
+-- look for plugin keyring 
+SELECT 
+    PLUGIN_NAME, PLUGIN_STATUS
+FROM
+    INFORMATION_SCHEMA.PLUGINS;
+
+-- files path for plugins 
+SHOW VARIABLES LIKE 'plugin_dir';
+
+-- install plugin
+INSTALL PLUGIN keyring_file SONAME 'keyring_file.dll';
+
+
+
+
+
+
+
+
+
---
+++ Lessons-brocode/2-tables.sql
@@ -0,0 +1,46 @@
+USE myDB;
+ 
+CREATE TABLE employees (
+    employee_id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    hourly_pay DECIMAL(5 , 2 ),
+    hire_date DATE
+);
+
+-- DROP TABLE EMPLOYEES; 
+
+SELECT 
+    *
+FROM
+    employees;
+    
+RENAME TABLE workers TO employees;
+
+-- add column phone_number 
+ALTER TABLE employees
+ADD phone_number VARCHAR(15);
+
+-- change name column phone_number
+ALTER TABLE employees
+RENAME COLUMN phone_number TO email;
+
+-- change data type of email after transition to phone_number
+ALTER TABLE employees
+MODIFY COLUMN email VARCHAR(100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
