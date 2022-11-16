543713003a84b523185cd7957a9b4bd8636d1da6
hoang duy <hoangduy1771@gmail.com>
2022-11-16 22:17:55 +0700

    modify useful commands file

--- Lessons-brocode/a-number-of-useful-command.sql
+++ Lessons-brocode/a-number-of-useful-command.sql
@@ -1,3 +1,6 @@
+-- Show all the constraint in a table
+SHOW CREATE TABLE products_notNull; 
+
 -- Select a few rows revealing the columns
 SELECT * FROM employees LIMIT 2;
 
@@ -8,9 +11,6 @@ DROP COLUMN phone_number;
 -- delete a duplicate row 
 DELETE FROM employees WHERE first_name='Eugene' LIMIT 1;
 
--- show all the constraint in a table
-SHOW CREATE TABLE products_notNull; 
-
 -- delete a row from a table 
 DELETE FROM transactions WHERE transaction_id = 1;
 
@@ -19,6 +19,8 @@ UPDATE transactions
 SET ammount = 500000.00
 WHERE transaction_id = 1;
 
+-- delete all rows from a table
+DELETE FROM transactions; 
 
 
 
