55338349dd2533178afa4bd34c33d97275bb2ab1
hduy <hoangduy1771@gmail.com>
2024-09-10 21:52:17 +0700

    add testmentor sql

--- MSQL server/testmentorsql.sql
+++ MSQL server/testmentorsql.sql
@@ -1,7 +1,28 @@
 
-create database QuanLyBanHang;
+create database StudentManagement;
 
-create table test (
-	id int primary key,
-	name varchar(50)
+
+create table subject (
+	SubjectId int primary key,
+	SubjectName varchar(50),
+	Hours int
 );
+
+create table course (
+	CourseId int primary key,
+	CourseName varchar(200)
+);
+
+create table class (
+	ClassId int primary key,
+	name varchar(50),
+	email varchar(50),
+	CourseId int,
+	foreign key (CourseId) references COURSE(CourseId)
+);
+
+create table student(
+	
+); 
+
+
